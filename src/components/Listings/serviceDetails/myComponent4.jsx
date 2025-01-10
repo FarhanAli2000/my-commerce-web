47c69f3cb8d891a9edf391140cee0a6f890d5f1b
ArfaDevelopers <aarfa.developers@gmail.com>
2025-01-10 23:06:58 +0500

    builde lasted

--- src/components/Listings/serviceDetails/myComponent4.jsx
+++ src/components/Listings/serviceDetails/myComponent4.jsx
@@ -1,47 +1,43 @@
 import React from "react";
-import { GalleryImg1, GalleryImg2, GalleryImg3, GalleryImg9, gallery_1_jpg, gallery_2_jpg, gallery_3_jpg, gallery_4_jpg, gallery_5_jpg, galleryimage_9 } from "../../imagepath";
+import {
+  GalleryImg1,
+  GalleryImg2,
+  GalleryImg3,
+  GalleryImg9,
+} from "../../imagepath";
 import { Link } from "react-router-dom";
-
-import { SlideshowLightbox } from "lightbox.js-react";
-import 'lightbox.js-react/dist/index.css'
+import Lightbox from "react-lightbox";
 
 const Roomsprofile = () => {
-      const galleryItems = [
-          { original: GalleryImg1 },
-          { original: GalleryImg2 },
-          { original: GalleryImg3 },
-          { original: GalleryImg9 },
-         
-          
-      ];
-  
-      return (
-        <div className="row">
-   
-    {galleryItems.map((item, index) => (
-   <div class="col-lg-3  col-md-3 col-sm-3" key={index}>
-   <div class="review-gallery">
-      <Link
-        to="#"
-        data-fancybox="gallery1"
-      >
-        <SlideshowLightbox>
-        <img 
-          className="img-fluid"
-          alt="Image"
-          src={item.original}
-        />
-        </SlideshowLightbox>
+  const galleryItems = [
+    { original: GalleryImg1 },
+    { original: GalleryImg2 },
+    { original: GalleryImg3 },
+    { original: GalleryImg9 },
+  ];
 
-        
-      </Link>
-      </div>
+  return (
+    <div className="row">
+      {galleryItems.map((item, index) => (
+        <div className="col-lg-3 col-md-3 col-sm-3" key={index}>
+          <div className="review-gallery">
+            <Link to="#" data-fancybox="gallery1">
+              <Lightbox
+                images={[{ src: item.original, title: `Image ${index + 1}` }]}
+                renderImage={(image, index) => (
+                  <img
+                    className="img-fluid"
+                    alt={`Image ${index + 1}`}
+                    src={image.src}
+                  />
+                )}
+              />
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
-    ))}
+  );
+};
 
-</div>
-
-
-      )
-    }
-    export default Roomsprofile;
+export default Roomsprofile;
